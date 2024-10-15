2ff346d5db4bdbe7b2455051c2222af69f5c1e86
Taylor Fox <tkayafox@gmail.com>
2024-10-14 23:14:44 -0600

    Rename Game to Shattered Protocols

---
+++ Shattered Protocols/Shattered Protocols/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shattered_Protocols
+{
+    internal class Program
+    {
+        ArrayList roomList = new ArrayList();
+
+        ///Starts the Game Logic
+        public void Main ()
+        {
+            Console.WriteLine("Hello, World!");
+
+            //  use the current time as a seed to ensure Randomness between program runs
+            int seed = DateTime.Now.Millisecond;
+            Random randomizer = new Random(seed);
+
+            //Start the Game Logic
+            Game game = new Game();
+        }
+    }
+}
