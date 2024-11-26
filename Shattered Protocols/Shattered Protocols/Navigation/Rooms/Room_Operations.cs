7ef7b77694f6dbb8d56c7c55beedf0bfff797837
Taylor Fox <tkayafox@gmail.com>
2024-11-25 18:13:09 -0700

    puzzle for operations set to null until flashdrive is used

--- Shattered Protocols/Shattered Protocols/Navigation/Rooms/Room_Operations.cs
+++ Shattered Protocols/Shattered Protocols/Navigation/Rooms/Room_Operations.cs
@@ -22,10 +22,6 @@ namespace Shattered_Protocols.Navigation.Rooms
     There seems to be a USB port in the casing. Huh... there is also a suspicious looking 
     avian symbol above the port...
              ";
-            RoomPuzzle = new PuzzleSQLInjection();
-
-            //Tell Puzzle to unlock all doors to THIS room
-            RoomPuzzle.RoomUnlock = RoomType.Room_Operations;
 
             //Define Neighboring Rooms
             //  DirectionEnum relative to current roon, type of room
@@ -38,8 +34,6 @@ namespace Shattered_Protocols.Navigation.Rooms
         /// <param name="originDirection"></param>
         public override void Enter(DirectionEnum originDirection)
         {
-            RoomPuzzle.RoomUnlock = RoomType.Room_Operations;
-
             //Display room name and description using ToString
             GameController.Output(ToString());
         }
@@ -58,6 +52,7 @@ namespace Shattered_Protocols.Navigation.Rooms
                 if (name.Equals("ducky_flash_drive", StringComparison.OrdinalIgnoreCase))
                 {
                     //Run ShowPuzzle Logic if applicable
+                    RoomPuzzle = new PuzzleSQLInjection();
                     ShowPuzzle();
                 }
             }
